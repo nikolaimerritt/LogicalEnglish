8a599f5618683d4c00abf0193325ff8484868547
Nikolai Merritt <nikolaimerritt20@gmail.com>
2022-06-15 11:28:00 +0100

    templates get auto-suggested, but code snippets do not work

--- language-server/logical-english/server/src/completions.ts
+++ language-server/logical-english/server/src/completions.ts
@@ -1,43 +1,43 @@
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { CompletionItem, CompletionItemKind, /* InsertReplaceEdit,*/ TextDocumentPositionParams } from "vscode-languageserver";
-// import { literalAtPosition, sectionRange, templatesInDocument } from './utils';
+import { CompletionItem, CompletionItemKind, /* InsertReplaceEdit,*/ TextDocumentPositionParams, InsertTextFormat } from "vscode-languageserver";
+import { literalAtPosition, sectionRange, templatesInDocument } from './utils';
 // import { InsertTextFormat } from 'vscode-languageclient';
 
 
 export function provideCompletions(document: TextDocument, params: TextDocumentPositionParams): CompletionItem[] {	
 	return [
-		// ...literalCompletion(document, params),
+		...literalCompletion(document, params),
 		...dummyCompletion()
 	];
 }
 
 
-// function literalCompletion(document: TextDocument, params: TextDocumentPositionParams): CompletionItem[] {
-// 	const knowledgeBaseRange = sectionRange('knowledge base', document);
-// 	if (knowledgeBaseRange === undefined 
-// 			|| params.position.line < knowledgeBaseRange.start.line 
-// 			|| params.position.line > knowledgeBaseRange.end.line) 
-// 		return [];
+function literalCompletion(document: TextDocument, params: TextDocumentPositionParams): CompletionItem[] {
+	const knowledgeBaseRange = sectionRange('knowledge base', document);
+	if (knowledgeBaseRange === undefined 
+			|| params.position.line < knowledgeBaseRange.start.line 
+			|| params.position.line > knowledgeBaseRange.end.line) 
+		return [];
 	
-// 	const templates = templatesInDocument(document);
+	const templates = templatesInDocument(document);
 
-// 	const line = document.getText().split('\n')[params.position.line];
-// 	const literal = literalAtPosition(line, params.position.character);
-// 	if (literal === undefined)
-// 		return [];
+	const line = document.getText().split('\n')[params.position.line];
+	const literal = literalAtPosition(line, params.position.character);
+	if (literal === undefined)
+		return [];
 	
-// 	const completions: CompletionItem[] = [];
-// 	templates.forEach(template => {
-// 		if (template.matchesIncompleteLiteral(literal))
-// 			completions.push({
-// 				label: template.toSnippet(),
-// 				kind: CompletionItemKind.Text,
-// 				insertTextFormat: InsertTextFormat.Snippet
-// 			});
-// 	});
+	const completions: CompletionItem[] = [];
+	templates.forEach(template => {
+		if (template.matchesIncompleteLiteral(literal))
+			completions.push({
+				label: template.toSnippet(),
+				kind: CompletionItemKind.Text,
+				insertTextFormat: InsertTextFormat.Snippet
+			});
+	});
 
-// 	return completions;
-// }
+	return completions;
+}
 
 function dummyCompletion(): CompletionItem[] {
 	return [
